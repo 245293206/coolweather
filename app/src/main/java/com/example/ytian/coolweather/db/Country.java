083ee53b459571fc5f867a74c78cb4e6f6234e05
Tony <245293206@qq.com>
2017-08-09 11:03:59 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/example/ytian/coolweather/db/Country.java
@@ -0,0 +1,46 @@
+package com.example.ytian.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by YTian on 2017/8/9.
+ */
+
+public class Country extends DataSupport {
+    private int id;
+    private String countryName;
+    private int weatherCode;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public int getWeatherCode() {
+        return weatherCode;
+    }
+
+    public void setWeatherCode(int weatherCode) {
+        this.weatherCode = weatherCode;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
